e3e14331e414ca56f1ab9a32fbafc79dacab3e3a
mistercle <mistercle@gmail.com>
2019-07-10 16:12:28 +0900

    test add_return_25_for_input_10_15 is passed

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -11,16 +11,16 @@ public class CustomCalculatorTest {
 
     //더하기 테스트 작성
     @Test
-    public void add() {
+    public void add_return_25_for_input_10_15() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(25));
         System.out.println("result :: " + result);
     }
 
     //빼기 테스트 작성
     @Test
-    public void subtract() {
+    public void subtract_return_13_for_input_23_10() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
         /* 이곳에 테스트 코드를 작성하세요. */
