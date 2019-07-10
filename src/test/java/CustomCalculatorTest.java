a1f7682280c8fcd884cea7e6e2f5d445ed1c037d
mistercle <mistercle@gmail.com>
2019-07-10 16:14:19 +0900

    test multiply_return_45_for_input_5_9 is passed

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -29,10 +29,10 @@ public class CustomCalculatorTest {
 
     //곱하기 테스트 작성
     @Test
-    public void multiply() {
+    public void multiply_return_45_for_input_5_9() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(45));
         System.out.println("result :: " + result);
     }
 
